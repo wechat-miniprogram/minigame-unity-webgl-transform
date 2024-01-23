19d5faa2b9fb56c67ced605c6c20e2cf3bf4cde9
wumeibanfa <904360463@qq.com>
2024-01-23 20:40:18 +0800

    wx.shareAppMessage-demo

---
+++ Demo/API_V2/Assets/API/Share/ShareAppMessage/ShareAppMessage.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using WeChatWASM;
+
+public class ShareAppMessage: Details
+{
+    // 测试 API
+    protected override void TestAPI(string[] args) {
+        Share();        
+    }
+
+    // 分享
+    private void Share() {
+        WX.ShareAppMessage(new ShareAppMessageOption() {
+            title = "小游戏分享"
+        });
+    }
+    
+}
