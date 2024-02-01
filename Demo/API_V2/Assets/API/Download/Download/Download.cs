85c94138a38108c5bb3f54ae0fc4f58f9a3dc967
wumeibanfa <904360463@qq.com>
2024-02-01 20:32:04 +0800

    下载-demo

---
+++ Demo/API_V2/Assets/API/Download/Download/Download.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using LitJson;
+using UnityEngine;
+using WeChatWASM;
+public class Download : Details
+{
+
+    private WXDownloadTask _downloadTask;
+
+    private readonly Action<DownloadTaskOnHeadersReceivedListenerResult> _onHeadersReceived = (res) => {
+        var result = "onHeadersReceived\n" + JsonMapper.ToJson(res);
+        GameManager.Instance.detailsController.AddResult(new ResultData()
+        {
+            initialContentText = result
+        });
+    };
+
+    private readonly Action<DownloadTaskOnProgressUpdateListenerResult> _onProgressUpdate = (res) => {
+        var result = "onProgressUpdate\n" + JsonMapper.ToJson(res);
+        GameManager.Instance.detailsController.AddResult(new ResultData()
+        {
+            initialContentText = result
+        });
+    };
+
+    // 测试 API
+    protected override void TestAPI(string[] args)
+    {
+        // 填入具体的url和其它参数
+        _downloadTask = WX.DownloadFile(new DownloadFileOption 
+        {
+            url = "xxxx",
+            success = (res) => {
+                Debug.Log(JsonMapper.ToJson(res));
+            },
+            fail = (res) => {
+                Debug.Log("fail: " + res.errMsg);
+            },
+            complete = (res) => {
+                Debug.Log("complete");
+            }
+        });
+
+        _downloadTask.OnHeadersReceived(_onHeadersReceived);
+        _downloadTask.OnProgressUpdate(_onProgressUpdate);
+    }
+
+    public void abort() {
+        _downloadTask.Abort();
+    }
+}
+
