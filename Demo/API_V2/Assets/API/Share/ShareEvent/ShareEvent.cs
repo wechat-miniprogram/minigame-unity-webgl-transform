82c79c8c565ecb3b350a42d12c47f40c18b4423f
wumeibanfa <904360463@qq.com>
2024-02-02 18:43:31 +0800

    转发-demo

---
+++ Demo/API_V2/Assets/API/Share/ShareEvent/ShareEvent.cs
@@ -0,0 +1,197 @@
+using System;
+using System.Collections.Generic;
+using LitJson;
+using UnityEngine;
+using WeChatWASM;
+
+public class ShareEvent : Details
+{
+
+    private bool _isListeningShareTimeline = false;
+
+    private readonly Action<Action<OnShareTimelineListenerResult>> _onShareTimelineCallback = (callback) => {
+        callback(new OnShareTimelineListenerResult 
+        {
+            imageUrl = "xxx",
+            imagePreviewUrl = "yy",
+            imagePreviewUrlId = "xx",
+            imageUrlId = "xxx",
+            path = "xx",
+            query = "xx",
+            title = "test",
+        });
+    };
+
+    private readonly Action<OnShareMessageToFriendListenerResult> _onShareMessageToFriend = (res) => {
+        var result = "onShareMessageToFriend\n" + JsonMapper.ToJson(res);
+        GameManager.Instance.detailsController.AddResult(new ResultData()
+        {
+            initialContentText = result
+        });
+    };
+
+    private readonly Action<Action<WXShareAppMessageParam>> _onShareAppMessageCallback = (callback) => {
+        callback(new WXShareAppMessageParam 
+        {
+            title = "转发标题",
+            imageUrl = "xx",
+            query = "key1=val1&key2=val2"
+        });
+    };
+
+
+    private void Start()
+    {
+        GameManager.Instance.detailsController.BindExtraButtonAction(0, showShareMenu);
+        GameManager.Instance.detailsController.BindExtraButtonAction(1, hideShareMenu);
+        GameManager.Instance.detailsController.BindExtraButtonAction(2, showShareImageMenu);
+        GameManager.Instance.detailsController.BindExtraButtonAction(3, setMessageToFriendQuery);
+        GameManager.Instance.detailsController.BindExtraButtonAction(4, setHandoffQuery);
+        GameManager.Instance.detailsController.BindExtraButtonAction(5, onShareTimeline);
+        GameManager.Instance.detailsController.BindExtraButtonAction(6, onShareMessageToFriend);
+        GameManager.Instance.detailsController.BindExtraButtonAction(7, onShareAppMessage);
+    }
+
+    // 测试 API
+    protected override void TestAPI(string[] args)
+    {
+       updateShareMenu();
+    }
+
+    public void updateShareMenu() {
+        var parameter = new UpdatableMessageFrontEndParameter[] {
+                new UpdatableMessageFrontEndParameter {
+                    name = "xxx",
+                    value = "yyy"
+                },
+                new UpdatableMessageFrontEndParameter {
+                    name = "zz",
+                    value = "kk"
+                }
+        };
+
+        var info = new UpdatableMessageFrontEndTemplateInfo{
+            parameterList = parameter
+        };
+
+        WX.UpdateShareMenu(new UpdateShareMenuOption
+        {
+            isPrivateMessage = true,
+            activityId = "xxx",
+            templateInfo = info,
+            success = (res) => {
+                WX.ShowToast(new ShowToastOption 
+                {
+                    title = "设置成功"
+                });
+            },
+            fail = (res) => {
+                Debug.Log("fail" + res.errMsg);
+            },
+            complete = (res) => {
+                Debug.Log("complete");
+            }
+        });
+    }
+
+    public void showShareMenu() {
+        WX.ShowShareMenu(new ShowShareMenuOption 
+        {
+            withShareTicket = true,
+            menus = new string[] {"shareAppMessage", "shareTimeline"},
+            success = (res) => {
+               Debug.Log("success");
+            },
+            fail = (res) => {
+                Debug.Log("fail" + res.errMsg);
+            },
+            complete = (res) => {
+                Debug.Log("complete");
+            }
+        });
+    }
+
+    public void hideShareMenu() {
+        WX.HideShareMenu(new HideShareMenuOption 
+        {
+            menus = new string[] {"shareAppMessage", "shareTimeline"},
+            success = (res) => {
+               Debug.Log("success");
+            },
+            fail = (res) => {
+                Debug.Log("fail" + res.errMsg);
+            },
+            complete = (res) => {
+                Debug.Log("complete");
+            }
+        });
+    }
+
+    public void showShareImageMenu() {
+        WX.DownloadFile(new DownloadFileOption 
+        {
+            url = "xxxxx",
+            success = (res) => {
+                WX.ShowShareImageMenu(new ShowShareImageMenuOption 
+                {
+                    path = res.tempFilePath,
+                    style = "default",
+                    success = (res) => {
+                        Debug.Log("success");
+                    },
+                    fail = (res) => {
+                        Debug.Log("fail" + res.errMsg);
+                    },
+                    complete = (res) => {
+                        Debug.Log("complete");
+                    }
+                });
+            }
+        });
+    }
+
+    public void setMessageToFriendQuery() {
+        var isSuccess = WX.SetMessageToFriendQuery(new SetMessageToFriendQueryOption 
+        {
+            shareMessageToFriendScene = 1,
+            query = "abcd"
+        });
+         WX.ShowToast(new ShowToastOption 
+        {
+            title = isSuccess ? "true" : "false" 
+        });
+    }
+
+    public void setHandoffQuery() {
+        var isSuccess = WX.SetHandoffQuery("xxx");
+         WX.ShowToast(new ShowToastOption 
+        {
+            title = isSuccess ? "true" : "false" 
+        });
+    }
+
+    public void onShareTimeline() {
+        if (!_isListeningShareTimeline) {
+            WX.OnShareTimeline(_onShareTimelineCallback);
+        } else {
+            WX.OffShareTimeline(_onShareTimelineCallback);
+        }
+        _isListeningShareTimeline = !_isListeningShareTimeline;
+        GameManager.Instance.detailsController.ChangeExtraButtonText(5, _isListeningShareTimeline ? "取消监听分享到朋友圈" : "开始监听分享到朋友圈");
+    }
+
+    public void onShareMessageToFriend() {
+        WX.OnShareMessageToFriend(_onShareMessageToFriend);
+    }
+
+    public void onShareAppMessage() {
+        var defaultParam = new WXShareAppMessageParam 
+        {
+            title = "转发标题",
+            imageUrl = "xx",
+            query = "key1=val1&key2=val2"
+        };
+        WX.OnShareAppMessage(defaultParam, _onShareAppMessageCallback);
+    }
+}
+
---
+++ Demo/API_V2/Assets/API/Share/ShareEvent2/ShareEvent2.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections.Generic;
+using LitJson;
+using UnityEngine;
+using WeChatWASM;
+
+public class ShareEvent2 : Details
+{
+
+    private bool _isListeningHandoff = false;
+    private bool _isListeningCopyUrl = false;
+    private bool _isListeningAddToFavorites = false;
+
+    private readonly Action<Action<OnHandoffListenerResult>> _onHandoff = (callback) => {
+        callback(new OnHandoffListenerResult 
+        {
+            query = "xxxx"
+        });
+    };
+
+    private readonly Action<Action<OnCopyUrlListenerResult>> _onCopyUrl = (callback) => {
+        callback(new OnCopyUrlListenerResult 
+        {
+            query = "xx"
+        });
+    };
+
+    private readonly Action<Action<OnAddToFavoritesListenerResult>> _onAddToFavorites = (callback) => {
+        callback(new OnAddToFavoritesListenerResult 
+        {
+            title = "收藏标题",
+            imageUrl = "xx",
+            query = "key1=val1&key2=val2",
+            disableForward = false
+        });
+    };
+
+    private void Start()
+    {
+        GameManager.Instance.detailsController.BindExtraButtonAction(0, onCopyUrl);
+        GameManager.Instance.detailsController.BindExtraButtonAction(1, onAddToFavorites);
+        GameManager.Instance.detailsController.BindExtraButtonAction(2, getShareInfo);
+        GameManager.Instance.detailsController.BindExtraButtonAction(3, authPrivateMessage);
+    }
+
+    // 测试 API
+    protected override void TestAPI(string[] args)
+    {
+       onHandoff();
+    }
+
+    public void onHandoff() {
+        if (!_isListeningHandoff) {
+            WX.OnHandoff(_onHandoff);
+        } else {
+            WX.OffHandoff(_onHandoff);
+        }
+        _isListeningHandoff = !_isListeningHandoff;
+        GameManager.Instance.detailsController.ChangeInitialButtonText(_isListeningHandoff ? "取消监听在电脑上打开" : "开始监听在电脑上打开");
+    }
+
+    public void onCopyUrl() {
+        if (!_isListeningCopyUrl) {
+            WX.OnCopyUrl(_onCopyUrl);
+        } else {
+            WX.OffCopyUrl(_onCopyUrl);
+        }
+        _isListeningCopyUrl = !_isListeningCopyUrl;
+        GameManager.Instance.detailsController.ChangeExtraButtonText(0, _isListeningCopyUrl ? "取消监听复制链接" : "开始监听复制链接");
+    } 
+    
+    public void onAddToFavorites() {
+        if (!_isListeningAddToFavorites) {
+            WX.OnAddToFavorites(_onAddToFavorites);
+        } else {
+            WX.OffAddToFavorites(_onAddToFavorites);
+        }
+        _isListeningAddToFavorites = !_isListeningAddToFavorites;
+        GameManager.Instance.detailsController.ChangeExtraButtonText(1, _isListeningAddToFavorites ? "取消监听收藏" : "开始监听收藏");
+    }
+
+    public void getShareInfo() {
+        WX.GetShareInfo(new GetShareInfoOption 
+        {
+            shareTicket = "xxx",
+            timeout = 2000,
+            success = (res) => {
+               Debug.Log("success");
+            },
+            fail = (res) => {
+                Debug.Log("fail" + res.errMsg);
+            },
+            complete = (res) => {
+                Debug.Log("complete");
+            }
+        });
+    }
+
+    public void authPrivateMessage() {
+        WX.AuthPrivateMessage(new AuthPrivateMessageOption 
+        {
+            shareTicket = "xxxxxx",
+            success = (res) => {
+               Debug.Log("authPrivateMessage success" + JsonMapper.ToJson(res));
+                // res
+                // {
+                //   errMsg: 'authPrivateMessage:ok'
+                //   valid: true
+                //   iv: 'xxxx',
+                //   encryptedData: 'xxxxxx'
+                // }
+            },
+            fail = (res) => {
+                Debug.Log("authPrivateMessage fail" + res.errMsg);
+            }
+        });
+    }
+}
+
