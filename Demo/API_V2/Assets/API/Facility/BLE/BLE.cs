b5a962f90cd12f9f2ab0ab506a40e47e9e4140a2
wumeibanfa <904360463@qq.com>
2024-02-04 15:56:25 +0800

    蓝牙相关api-demo

---
+++ Demo/API_V2/Assets/API/Facility/BLE/BLE.cs
@@ -0,0 +1,237 @@
+using System;
+using System.Collections.Generic;
+using LitJson;
+using UnityEngine;
+using WeChatWASM;
+public class BLE : Details
+{
+
+    private bool _isListening = false;
+
+    private readonly Action<OnBLEMTUChangeListenerResult> _onBLEMTUChange = (res) => {
+        var result = "onBLEMTUChange\n" + JsonMapper.ToJson(res);
+        GameManager.Instance.detailsController.AddResult(new ResultData()
+        {
+            initialContentText = result
+        });
+    };
+
+    private readonly Action<OnBLEConnectionStateChangeListenerResult> _onBLEConnectionStateChange = (res) => {
+        var result = "onBLEConnectionStateChange\n" + JsonMapper.ToJson(res);
+        GameManager.Instance.detailsController.AddResult(new ResultData()
+        {
+            initialContentText = result
+        });
+    };
+
+    private readonly Action<OnBLECharacteristicValueChangeListenerResult> _onBLECharacteristicValueChange = (res) => {
+        var result = "onBLECharacteristicValueChange\n" + JsonMapper.ToJson(res);
+        GameManager.Instance.detailsController.AddResult(new ResultData()
+        {
+            initialContentText = result
+        });
+    };
+
+
+    private void Start()
+    {
+        // 监听蓝牙低功耗的最大传输单元变化事件（仅安卓触发）
+        WX.OnBLEMTUChange(_onBLEMTUChange);
+
+        // 监听蓝牙低功耗连接状态改变事件。包括开发者主动连接或断开连接，设备丢失，连接异常断开等等
+        WX.OnBLEConnectionStateChange(_onBLEConnectionStateChange);
+
+        // 监听蓝牙低功耗设备的特征值变化事件。
+        WX.OnBLECharacteristicValueChange(_onBLECharacteristicValueChange);
+
+        GameManager.Instance.detailsController.BindExtraButtonAction(0, writeBLECharacteristicValue);
+        GameManager.Instance.detailsController.BindExtraButtonAction(1, setBLEMTU);
+        GameManager.Instance.detailsController.BindExtraButtonAction(2, readBLECharacteristicValue);
+        GameManager.Instance.detailsController.BindExtraButtonAction(3, notifyBLECharacteristicValueChange);
+        GameManager.Instance.detailsController.BindExtraButtonAction(4, getBLEMTU);
+        GameManager.Instance.detailsController.BindExtraButtonAction(5, getBLEDeviceServices);
+        GameManager.Instance.detailsController.BindExtraButtonAction(6, getBLEDeviceRSSI);
+        GameManager.Instance.detailsController.BindExtraButtonAction(7, getBLEDeviceCharacteristics);
+    }
+
+    // 测试 API
+    protected override void TestAPI(string[] args)
+    {
+        createBLEConnection();
+    }
+
+    public void createBLEConnection() {
+        if (!_isListening) {
+            WX.CreateBLEConnection(new CreateBLEConnectionOption 
+            {
+                deviceId = "xxx",
+                timeout = 20000,
+                success = (res) => {
+                    Debug.Log("success " + JsonMapper.ToJson(res));
+                },
+                fail = (res) => {
+                    Debug.Log("fail" + res.errMsg);
+                },
+                complete = (res) => {
+                    Debug.Log("complete");
+                }
+            });
+        } else {
+            WX.CloseBLEConnection(new CloseBLEConnectionOption 
+            {
+                deviceId = "xxx",
+                success = (res) => {
+                    Debug.Log("success " + JsonMapper.ToJson(res));
+                },
+                fail = (res) => {
+                    Debug.Log("fail" + res.errMsg);
+                },
+                complete = (res) => {
+                    Debug.Log("complete");
+                }
+            });
+        }
+        _isListening = !_isListening;
+        GameManager.Instance.detailsController.ChangeInitialButtonText(_isListening ? "断开连接" : "开始连接");
+    }
+
+    // 目前会报param.value类型错误，已知问题，等待修复
+    public void writeBLECharacteristicValue() {
+        WX.WriteBLECharacteristicValue(new WriteBLECharacteristicValueOption 
+        {
+            deviceId = "xxx",
+            serviceId = "xxx",
+            characteristicId = "xxx",
+            value = new byte[] {1, 2, 3},
+            success = (res) => {
+                Debug.Log("success " + JsonMapper.ToJson(res));
+            },
+            fail = (res) => {
+                Debug.Log("fail" + res.errMsg);
+            },
+            complete = (res) => {
+                Debug.Log("complete");
+            }
+        });
+    }
+
+    public void setBLEMTU() {
+        WX.SetBLEMTU(new SetBLEMTUOption 
+        {
+            deviceId = "xx",
+            mtu = 100,
+            success = (res) => {
+                Debug.Log("success " + JsonMapper.ToJson(res));
+            },
+            fail = (res) => {
+                Debug.Log("fail" + res.mtu);
+            },
+            complete = (res) => {
+                Debug.Log("complete");
+            }
+        });
+    }
+
+    public void readBLECharacteristicValue() {
+        WX.ReadBLECharacteristicValue(new ReadBLECharacteristicValueOption 
+        {
+            deviceId = "xx",
+            serviceId = "xx",
+            characteristicId = "xx",
+            success = (res) => {
+                Debug.Log("success " + JsonMapper.ToJson(res));
+            },
+            fail = (res) => {
+                Debug.Log("fail" + res.errMsg);
+            },
+            complete = (res) => {
+                Debug.Log("complete");
+            }
+        });
+    }
+
+    public void notifyBLECharacteristicValueChange() {
+        WX.NotifyBLECharacteristicValueChange(new NotifyBLECharacteristicValueChangeOption {
+            deviceId = "xx",
+            serviceId = "xx",
+            characteristicId = "xx",
+            state = true,
+            type = "indication",
+            success = (res) => {
+                Debug.Log("success " + JsonMapper.ToJson(res));
+            },
+            fail = (res) => {
+                Debug.Log("fail" + res.errMsg);
+            },
+            complete = (res) => {
+                Debug.Log("complete");
+            }
+        });
+    }
+
+    public void getBLEMTU() {
+        WX.GetBLEMTU(new GetBLEMTUOption 
+        {
+            deviceId = "xx",
+            success = (res) => {
+                Debug.Log("success " + JsonMapper.ToJson(res));
+            },
+            fail = (res) => {
+                Debug.Log("fail" + res.errMsg);
+            },
+            complete = (res) => {
+                Debug.Log("complete");
+            }
+        });
+    }
+
+    public void getBLEDeviceServices() {
+        WX.GetBLEDeviceServices(new GetBLEDeviceServicesOption 
+        {
+            deviceId = "xx",
+            success = (res) => {
+                Debug.Log("success " + JsonMapper.ToJson(res));
+            },
+            fail = (res) => {
+                Debug.Log("fail" + res.errMsg);
+            },
+            complete = (res) => {
+                Debug.Log("complete");
+            }
+        });
+    }
+
+    public void getBLEDeviceRSSI() {
+        WX.GetBLEDeviceRSSI(new GetBLEDeviceRSSIOption 
+        {
+            deviceId = "xx",
+            success = (res) => {
+                Debug.Log("success " + JsonMapper.ToJson(res));
+            },
+            fail = (res) => {
+                Debug.Log("fail" + res.errMsg);
+            },
+            complete = (res) => {
+                Debug.Log("complete");
+            }
+        });
+    }
+
+    public void getBLEDeviceCharacteristics() {
+        WX.GetBLEDeviceCharacteristics(new GetBLEDeviceCharacteristicsOption 
+        {
+            deviceId = "xx",
+            serviceId = "xx",
+            success = (res) => {
+                Debug.Log("success " + JsonMapper.ToJson(res));
+            },
+            fail = (res) => {
+                Debug.Log("fail" + res.errMsg);
+            },
+            complete = (res) => {
+                Debug.Log("complete");
+            }
+        });
+    }
+}
+
---
+++ Demo/API_V2/Assets/API/Facility/BLEPeripheral/BLEPeripheral.cs
@@ -0,0 +1,239 @@
+using System;
+using System.Collections.Generic;
+using LitJson;
+using UnityEngine;
+using WeChatWASM;
+public class BLEPeripheral : Details
+{
+
+    private bool _isListening = false;
+    private BLEPeripheralServer _server;
+
+    private readonly Action<OnBLEPeripheralConnectionStateChangedListenerResult> _onBLEPeripheralConnectionStateChanged = (res) => {
+        var result = "onBLEPeripheralConnectionStateChanged\n" + JsonMapper.ToJson(res);
+        GameManager.Instance.detailsController.AddResult(new ResultData()
+        {
+            initialContentText = result
+        });
+    };
+
+    private readonly Action<OnCharacteristicReadRequestListenerResult> _onCharacteristicReadRequest = (res) => {
+        var result = "onCharacteristicReadRequest\n" + JsonMapper.ToJson(res);
+        GameManager.Instance.detailsController.AddResult(new ResultData()
+        {
+            initialContentText = result
+        });
+    };
+
+    private readonly Action<OnCharacteristicSubscribedListenerResult> _onCharacteristicSubscribed = (res) => {
+        var result = "onCharacteristicSubscribed\n" + JsonMapper.ToJson(res);
+        GameManager.Instance.detailsController.AddResult(new ResultData()
+        {
+            initialContentText = result
+        });
+    };
+
+    private readonly Action<OnCharacteristicSubscribedListenerResult> _onCharacteristicUnsubscribed = (res) => {
+        var result = "onCharacteristicUnsubscribed\n" + JsonMapper.ToJson(res);
+        GameManager.Instance.detailsController.AddResult(new ResultData()
+        {
+            initialContentText = result
+        });
+    };
+
+    private readonly Action<OnCharacteristicWriteRequestListenerResult> _onCharacteristicWriteRequest = (res) => {
+        var result = "onCharacteristicWriteRequest\n" + JsonMapper.ToJson(res);
+        GameManager.Instance.detailsController.AddResult(new ResultData()
+        {
+            initialContentText = result
+        });
+    };
+
+    private void Start()
+    {
+        WX.CreateBLEPeripheralServer(new CreateBLEPeripheralServerOption 
+        {
+            success = (res) => {
+                Debug.Log("success " + JsonMapper.ToJson(res));
+                _server = res.server;
+            },
+            fail = (res) => {
+                Debug.Log("fail" + res.errMsg);
+            },
+            complete = (res) => {
+                Debug.Log("complete");
+            }
+        });
+
+        // 监听当前外围设备被连接或断开连接事件
+        WX.OnBLEPeripheralConnectionStateChanged(_onBLEPeripheralConnectionStateChanged);
+
+        GameManager.Instance.detailsController.BindExtraButtonAction(0, removeService);
+        GameManager.Instance.detailsController.BindExtraButtonAction(1, startAdvertising);
+        GameManager.Instance.detailsController.BindExtraButtonAction(2, stopAdvertising);
+        GameManager.Instance.detailsController.BindExtraButtonAction(3, writeCharacteristicValue);
+        GameManager.Instance.detailsController.BindExtraButtonAction(4, listen);
+    }
+
+    // 测试 API
+    protected override void TestAPI(string[] args)
+    {
+        addService();
+    }
+
+    public void addService() {
+        var permission1 = new CharacteristicPermission {
+            readable = true,
+            writeable = true
+        };
+
+        var properties1 = new CharacteristicProperties {
+            write = true,
+            read = true,
+            notify = true,
+            indicate = true
+        };
+
+        var descriptorpermission1 = new DescriptorPermission {
+            read = true,
+            write = true,
+        };
+
+        var descriptors1 = new Descriptor {
+            uuid = "zz",
+            permission = descriptorpermission1,
+            value = new byte[] {1, 2, 3}
+        };
+
+        var characteristics1 = new Characteristic {
+            uuid = "yy",
+            descriptors = new Descriptor[] { descriptors1 },
+            permission = permission1,
+            properties = properties1,
+        };
+
+       var service1 = new BLEPeripheralService {
+            uuid = "xx",
+            characteristics = new Characteristic[] { characteristics1 }
+       };
+
+       _server.addService(new AddServiceOption 
+       {
+            service = service1,
+            success = (res) => {
+                Debug.Log("success " + JsonMapper.ToJson(res));
+            },
+            fail = (res) => {
+                Debug.Log("fail" + res.errMsg);
+            },
+            complete = (res) => {
+                Debug.Log("complete");
+            }
+       });
+    }
+
+    public void removeService() {
+        _server.removeService(new RemoveServiceOption 
+        {
+            serviceId = "xx",
+            success = (res) => {
+                Debug.Log("success " + JsonMapper.ToJson(res));
+            },
+            fail = (res) => {
+                Debug.Log("fail" + res.errMsg);
+            },
+            complete = (res) => {
+                Debug.Log("complete");
+            }
+        });
+    }
+
+    public void startAdvertising() {
+        var beacon1 = new BeaconInfoObj {
+            major = 100,
+            minor = 90,
+            uuid = "zzzzz",
+            measuredPower = 1000
+        };
+
+        var manufacturerData1 = new ManufacturerData {
+            manufacturerId = "0x1111",
+            manufacturerSpecificData = new byte[] {1, 2, 3}
+        };
+
+        var advertiseRequest1 = new AdvertiseReqObj {
+            beacon = beacon1,
+            connectable = true,
+            deviceName = "x",
+            manufacturerData = new ManufacturerData[] { manufacturerData1 },
+            serviceUuids = new string[] {"xx", "yy", "aa"}
+        };
+
+        _server.startAdvertising(new StartAdvertisingObject 
+        {
+            advertiseRequest = advertiseRequest1,
+            powerLevel = "medium",
+            success = (res) => {
+                Debug.Log("success " + JsonMapper.ToJson(res));
+            },
+            fail = (res) => {
+                Debug.Log("fail" + res.errMsg);
+            },
+            complete = (res) => {
+                Debug.Log("complete");
+            }
+        });
+    }
+
+    public void stopAdvertising() {
+        _server.stopAdvertising(new StopAdvertisingOption 
+        {
+            success = (res) => {
+                Debug.Log("success " + JsonMapper.ToJson(res));
+            },
+            fail = (res) => {
+                Debug.Log("fail" + res.errMsg);
+            },
+            complete = (res) => {
+                Debug.Log("complete");
+            }
+        });
+    }
+
+    public void writeCharacteristicValue() {
+        _server.writeCharacteristicValue(new WriteCharacteristicValueObject 
+        {
+            characteristicId = "xx",
+            needNotify = true,
+            serviceId = "yy",
+            value = new byte[] {1, 2, 3},
+            callbackId = 100,
+            success = (res) => {
+                Debug.Log("success " + JsonMapper.ToJson(res));
+            },
+            fail = (res) => {
+                Debug.Log("fail" + res.errMsg);
+            },
+            complete = (res) => {
+                Debug.Log("complete");
+            }
+        });
+    }
+
+    public void listen() {
+        if (!_isListening) {
+            _server.onCharacteristicReadRequest(_onCharacteristicReadRequest);
+            _server.onCharacteristicSubscribed(_onCharacteristicSubscribed);
+            _server.onCharacteristicUnsubscribed(_onCharacteristicUnsubscribed);
+            _server.onCharacteristicWriteRequest(_onCharacteristicWriteRequest);
+        } else {
+            _server.offCharacteristicReadRequest(_onCharacteristicReadRequest);
+            _server.offCharacteristicSubscribed(_onCharacteristicSubscribed);
+            _server.offCharacteristicUnsubscribed(_onCharacteristicUnsubscribed);
+            _server.offCharacteristicWriteRequest(_onCharacteristicWriteRequest);
+        }
+        _isListening = !_isListening;
+        GameManager.Instance.detailsController.ChangeInitialButtonText(_isListening ? "取消监听" : "开始监听");
+    }
+}
+
