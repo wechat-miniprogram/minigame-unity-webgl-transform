ae4112afa8d9cb04123e906a13d114d014cb391a
hollislu <hollislu@tencent.com>
2024-03-29 14:38:47 +0800

    fix:修复中文字体不显示

--- Demo/API_V2/Assets/Scripts/DetailsController.cs
+++ Demo/API_V2/Assets/Scripts/DetailsController.cs
@@ -8,32 +8,32 @@ public class DetailsController : MonoBehaviour
 {
     [Header("Entry Data")]
     [SerializeField] private EntrySO entrySO;
-    
+
     [Header("Elements")]
     [SerializeField] private GameObject optionPrefab;
     [SerializeField] private Transform optionsTransform;
-    
+
     [Header("Text")]
     [SerializeField] private Text titleText;
     [SerializeField] private Text APIText;
     [SerializeField] private Text descriptionText;
     [SerializeField] private Text startButtonText;
-    
+
     [Header("Button")]
     [SerializeField] private Button initialButton;
     [SerializeField] private GameObject buttonBlockPrefab;
     [SerializeField] private Transform buttonsTransform;
     [HideInInspector] public List<GameObject> extraButtonBlockObjects;
-    
+
     [Header("Result")]
     [SerializeField] private GameObject resultPrefab;
     [SerializeField] private Transform resultsTransform;
     [HideInInspector] public List<GameObject> resultObjects;
-    
-    [Header("Title Transform")] 
+
+    [Header("Title Transform")]
     [SerializeField] private RectTransform titleTransform;
     [SerializeField] private RectTransform backButtonTransform;
-    
+
     private Details _details;
 
     private void Start()
@@ -43,7 +43,7 @@ public class DetailsController : MonoBehaviour
         float height = (float)res.height;
         float safeArea = (float)GameManager.Instance.systemInfo.safeArea.top;
         // 根据系统安全区域调整标题和返回按钮的位置
-        titleTransform.anchoredPosition = new Vector2(titleTransform.anchoredPosition.x,  -(float)((res.top + res.height / 4) * info.pixelRatio));
+        titleTransform.anchoredPosition = new Vector2(titleTransform.anchoredPosition.x, -(float)((res.top + res.height / 4) * info.pixelRatio));
         backButtonTransform.anchoredPosition = new Vector2(backButtonTransform.anchoredPosition.x, -(float)((res.top + res.height / 4) * info.pixelRatio));
     }
 
@@ -52,14 +52,14 @@ public class DetailsController : MonoBehaviour
     {
         // 销毁详情信息
         Destroy(_details);
-        
+
         // 清除选项
         for (var i = optionsTransform.childCount - 1; i >= 0; i--)
         {
             var child = optionsTransform.GetChild(i);
             Destroy(child.gameObject);
         }
-        
+
         // 清除按钮
         initialButton.onClick.RemoveAllListeners();
         foreach (var i in extraButtonBlockObjects)
@@ -71,29 +71,78 @@ public class DetailsController : MonoBehaviour
         // 清除结果
         RemoveAllResult();
     }
-    
+
+    // 获取gap block并修改其最低高度
+    public void getGapBlock(GameObject parentGameObject)
+    {
+        var canvas = parentGameObject.GetComponentInParent<Canvas>();
+        Transform foreTransform = canvas.transform.Find("Foreground");
+        if (foreTransform != null)
+        {
+            Transform scrollViewTransform = foreTransform.Find("Scroll View");
+            if (scrollViewTransform != null)
+            {
+                Transform viewportTransform = scrollViewTransform.Find("Viewport");
+                if (viewportTransform != null)
+                {
+                    Transform contentTransform = viewportTransform.Find("Content");
+                    if (contentTransform != null)
+                    {
+                        Transform[] gapBlockTransforms = new Transform[2];
+                        gapBlockTransforms[0] = contentTransform.Find("Gap Block");
+                        gapBlockTransforms[1] = contentTransform.Find("Gap Block2");
+
+                        foreach (Transform gapBlockTransform in gapBlockTransforms)
+                        {
+                            if (gapBlockTransform != null)
+                            {
+                                GameObject gapBlockObject = gapBlockTransform.gameObject;
+                                LayoutElement layoutElement = gapBlockObject.GetComponent<LayoutElement>();
+                                layoutElement.minHeight = 150;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     // 初始化详情信息
     public void Init(EntrySO so)
     {
         ClearDetails();
-        
+
         entrySO = so;
 
         titleText.text = so.entryName;
         APIText.text = so.entryAPI;
         descriptionText.text = so.entryDescription;
 
+        // 限制 Text 的宽度
+        Transform parentTransform = descriptionText.transform.parent;
+        GameObject parentGameObject = parentTransform.gameObject;
+        LayoutElement layoutElement = parentGameObject.GetComponent<LayoutElement>();
+        if (layoutElement != null)
+        {
+            layoutElement.ignoreLayout = true;
+
+            RectTransform rectTransform = parentGameObject.GetComponent<RectTransform>();
+            rectTransform.sizeDelta = new Vector2(1000f, rectTransform.sizeDelta.y);
+
+            getGapBlock(parentGameObject);
+        }
+
         _details = (Details)gameObject.AddComponent(entrySO.EntryScriptType);
         _details.Init(entrySO);
-        
+
         // 生成选项
-        for(var i = 0; i < entrySO.optionList.Count; i++)
+        for (var i = 0; i < entrySO.optionList.Count; i++)
         {
             var optionObj = Instantiate(optionPrefab, optionsTransform);
             optionObj.name = entrySO.optionList[i].optionName;
             optionObj.GetComponentInChildren<OptionDropdownHandler>().Init(_details, i);
         }
-        
+
         // 设置初始按钮
         ChangeInitialButtonText(entrySO.initialButtonText);
         initialButton.onClick.AddListener(() =>
@@ -114,26 +163,26 @@ public class DetailsController : MonoBehaviour
         extraButton.GetComponentInChildren<Text>().text = "";
         Color transparentColor = new Color(0, 0, 0, 0);
         extraButton.GetComponentInChildren<Button>().GetComponent<Image>().color = transparentColor;
-        
+
         // 生成结果
         foreach (var result in entrySO.initialResultList)
         {
             AddResult(result);
         }
     }
-    
+
     // 更改初始按钮的文本
     public void ChangeInitialButtonText(string text)
     {
         startButtonText.text = text;
     }
-    
+
     // 更改额外按钮的文本
     public void ChangeExtraButtonText(int index, string text)
     {
         extraButtonBlockObjects[index].GetComponent<ButtonController>().ChangeButtonText(text);
     }
-    
+
     // 绑定额外按钮的操作
     public void BindExtraButtonAction(int index, UnityAction action)
     {
@@ -142,11 +191,12 @@ public class DetailsController : MonoBehaviour
     }
 
     // 获取初始按钮左上角距离画布左上角的相对距离
-    public Vector2 GetInitialButtonPosition() {   
+    public Vector2 GetInitialButtonPosition()
+    {
         var button = initialButton;
         var canvas = button.GetComponentInParent<Canvas>();
 
-               // 获取 Canvas 和按钮的 RectTransform 组件
+        // 获取 Canvas 和按钮的 RectTransform 组件
         RectTransform canvasRectTransform = canvas.GetComponent<RectTransform>();
         RectTransform buttonRectTransform = button.GetComponent<RectTransform>();
 
@@ -166,7 +216,8 @@ public class DetailsController : MonoBehaviour
     }
 
     // 获取初始按钮的长宽
-    public Vector2 GetInitialButtonSize() {
+    public Vector2 GetInitialButtonSize()
+    {
         return initialButton.GetComponent<RectTransform>().sizeDelta;
     }
 
@@ -175,7 +226,7 @@ public class DetailsController : MonoBehaviour
     {
         var resultObj = Instantiate(resultPrefab, resultsTransform);
         resultObjects.Add(resultObj);
-        
+
         ChangeResultTitle(resultObjects.Count - 1, resultData.initialTitleText);
         ChangeResultContent(resultObjects.Count - 1, resultData.initialContentText);
         if (resultData.isDisableInitially)
@@ -184,7 +235,7 @@ public class DetailsController : MonoBehaviour
         }
         return resultObj;
     }
-    
+
     // 移除所有结果信息
     public void RemoveAllResult()
     {
@@ -203,10 +254,10 @@ public class DetailsController : MonoBehaviour
         {
             Destroy(resultObjects[i]);
         }
-        
+
         resultObjects.RemoveRange(n, resultObjects.Count - n);
     }
-    
+
     // 设置结果的活跃状态
     public void SetResultActive(int index, bool isActive)
     {
@@ -218,7 +269,7 @@ public class DetailsController : MonoBehaviour
     {
         resultObjects[index].GetComponent<ResultController>().ChangeTitle(title);
     }
-    
+
     // 更改结果的内容
     public void ChangeResultContent(int index, string content)
     {
