9a8ef78f9ac893c568a5959f4f28a9e51a9a9bc5
v_shzhiwang <106916369+VincentWSZ@users.noreply.github.com>
2025-01-17 13:12:14 +0800

    add ToTempFilePathSync

--- Demo/API_V2/Assets/API/Render/ToTempFilePath/ToTempFilePath.cs
+++ Demo/API_V2/Assets/API/Render/ToTempFilePath/ToTempFilePath.cs
@@ -2,13 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using WeChatWASM;
+using LitJson;
 
 public class ToTempFilePath : Details
 {
     protected override void TestAPI(string[] args)
     {
-        LoadCanvasToTempFilePath();
+        if (args[8] == "同步执行")
+        {
+            LoadCanvasToTempFilePathSync();
+        }
+        else
+        {
+            LoadCanvasToTempFilePath();
+        }
     }
+
+    // 异步
     private void LoadCanvasToTempFilePath()
     {// 根据options数组的索引获取值
         float x = GetOptionValue(0);
@@ -37,7 +47,7 @@ public class ToTempFilePath : Details
             {
                 WX.ShowModal(new ShowModalOption()
                 {
-                    title = "截图成功",
+                    title = "截图成功（异步）",
                     content = $"{optionsInfo}\n\n临时文件路径：{result.tempFilePath}",
                     showCancel = false,
                     success = (res) =>
@@ -53,19 +63,6 @@ public class ToTempFilePath : Details
                             fail = (res) =>
                             {
                                 Debug.Log("预览失败");
-                            }
-                        });
-                        var fileManager = WX.GetFileSystemManager();
-                        fileManager.Access(new AccessParam()
-                        {
-                            path = result.tempFilePath,
-                            success = (res) =>
-                            {
-                                Debug.Log("文件存在");
-                            },
-                            fail = (res) =>
-                            {
-                                Debug.Log("文件不存在");
                             }
                         });
                     }
@@ -84,6 +81,58 @@ public class ToTempFilePath : Details
             {
                 Debug.Log("complete");
             },
+        });
+    }
+
+    // 同步
+    private void LoadCanvasToTempFilePathSync()
+    {
+
+        // 根据options数组的索引获取值
+        float x = GetOptionValue(0);
+        float y = GetOptionValue(1);
+        float width = GetOptionValue(2);
+        float height = GetOptionValue(3);
+        float destWidth = GetOptionValue(4);
+        float destHeight = GetOptionValue(5);
+        string fileType = GetOptionString(6, "png");
+        float quality = GetOptionValue(7);
+
+        string optionsInfo = $"当前参数值:\nx={x}\ny={y}\nwidth={width}\nheight={height}\ndestWidth={destWidth}\ndestHeight={destHeight}\nfileType={fileType}\nquality={quality}";
+
+        var tempFilePath = WXCanvas.ToTempFilePathSync(new WXToTempFilePathSyncParam()
+        {
+            x = (int)x,
+            y = (int)y,
+            width = (int)width,
+            height = (int)height,
+            destWidth = (int)destWidth,
+            destHeight = (int)destHeight,
+            fileType = fileType,
+            quality = (int)quality,
+        });
+        // 显示同步访问的结果
+        WX.ShowModal(new ShowModalOption()
+        {
+            title = "截图成功（同步）",
+            content = $"{optionsInfo}\n\n临时文件路径：{tempFilePath}",
+            showCancel = false,
+            success = (res) =>
+            {
+                WX.PreviewMedia(new PreviewMediaOption()
+                {
+                    sources = new[] { new MediaSource { url = tempFilePath, type = "image" } },
+                    current = 0,
+                    success = (res) =>
+                    {
+                        Debug.Log("预览成功");
+                    },
+                    fail = (res) =>
+                    {
+                        Debug.Log("预览失败");
+                    }
+                });
+            }
         });
     }
 }
