7e4b610d9648b9c4efd355566659241daf76c4b1
v_shzhiwang <106916369+VincentWSZ@users.noreply.github.com>
2025-03-18 15:59:58 +0800

    update 本地路径视频示例优化

--- Demo/API_V2/Assets/API/Video/PlayLocalFileVideo.cs
+++ Demo/API_V2/Assets/API/Video/PlayLocalFileVideo.cs
@@ -9,6 +9,7 @@ using WeChatWASM;
 public class PlayLocalFileVideo : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
+    private string localVideoPath;
     
     private void Awake()
     {
@@ -22,10 +23,51 @@ public class PlayLocalFileVideo : MonoBehaviour
         // 配置VideoPlayer
         videoPlayer.playOnAwake = false;
         videoPlayer.isLooping = false;
+        
+        // 设置本地视频路径
+        localVideoPath = WX.env.USER_DATA_PATH + "/video.mp4";
+    }
+
+    private void CheckAndPlayVideo()
+    {
+        // 获取文件系统管理器
+        var fs = WX.GetFileSystemManager();
+        
+        // 使用微信文件系统的方法检查文件是否存在
+        fs.Access(new AccessParam()
+        {
+            path = localVideoPath,
+            success = (res) => 
+            {
+                Debug.Log("本地视频文件已存在，直接播放");
+                // 文件存在，直接播放
+                LoadAndPlayVideo(localVideoPath);
+            },
+            fail = (res) => 
+            {
+                Debug.Log("本地视频文件不存在，需要下载: " + res.errMsg);
+                // 文件不存在，需要下载
+                DownloadFileVideo();
+            }
+        });
     }
 
     private void DownloadFileVideo()
     {
+        WX.ShowLoading(new ShowLoadingOption()
+        {
+            title = "视频下载中...",
+            mask = true,
+            success = (res) =>
+            {
+                Debug.Log("Loading indicator shown successfully");
+            },
+            fail = (res) =>
+            {
+                Debug.Log("Failed to show loading indicator: " + res.errMsg);
+            },
+        });
+        
         WX.DownloadFile(new DownloadFileOption()
         {
             url = "https://res.wx.qq.com/wechatgame/product/webpack/userupload/20190812/video.mp4",
@@ -36,17 +78,35 @@ public class PlayLocalFileVideo : MonoBehaviour
                 {
                     Debug.Log(res.tempFilePath);
                     var fs = WX.GetFileSystemManager();
-                    var filePath = fs.SaveFileSync(res.tempFilePath, WX.env.USER_DATA_PATH + "/video.mp4");
+                    var filePath = fs.SaveFileSync(res.tempFilePath, localVideoPath);
                     LoadAndPlayVideo(filePath);
                 }
             },
             fail = (res) =>
             {
-                Debug.Log("WX.DownloadFile fail");
+                Debug.Log("WX.DownloadFile fail: " + res.errMsg);
+                HideLoadingIndicator();
             },
             complete = (res) =>
             {
                 Debug.Log("WX.DownloadFile complete");
+                HideLoadingIndicator();
+            }
+        });
+    }
+    
+    // 隐藏加载提示的方法
+    private void HideLoadingIndicator()
+    {
+        WX.HideLoading(new HideLoadingOption()
+        {
+            success = (res) =>
+            {
+                Debug.Log("Loading indicator hidden successfully");
+            },
+            fail = (res) =>
+            {
+                Debug.LogError("Failed to hide loading indicator: " + res.errMsg);
             }
         });
     }
@@ -78,7 +138,8 @@ public class PlayLocalFileVideo : MonoBehaviour
     
     private void Start()
     {
-        DownloadFileVideo();
+        // 检查本地是否有文件，如果没有则下载
+        CheckAndPlayVideo();
     }
 
     private void OnDestroy()
