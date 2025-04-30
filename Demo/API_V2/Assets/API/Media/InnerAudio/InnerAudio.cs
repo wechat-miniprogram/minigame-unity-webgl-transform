29168e94528c58b26fad99e65fbf37a07cec445a
v_shanxia <v_shanxia@tencent.com>
2025-04-30 09:55:08 +0800

    fix：音频示例更改

--- Demo/API_V2/Assets/API/Media/InnerAudio/InnerAudio.cs
+++ Demo/API_V2/Assets/API/Media/InnerAudio/InnerAudio.cs
@@ -6,9 +6,18 @@ using WeChatWASM;
 
 public class InnerAudio : Details
 {
+    WXInnerAudioContext music;
+    private void Start()
+    {
+        // 绑定额外的按钮操作
+        GameManager.Instance.detailsController.BindExtraButtonAction(0, pause);
+        GameManager.Instance.detailsController.BindExtraButtonAction(1, stop);
+        GameManager.Instance.detailsController.BindExtraButtonAction(2, seek);
+    }
+
     protected override void TestAPI(string[] args)
     {
-        var music = WX.CreateInnerAudioContext(new InnerAudioContextParam() {
+        music = WX.CreateInnerAudioContext(new InnerAudioContextParam() {
            src = "https://res.wx.qq.com/wechatgame/product/webpack/userupload/20220901/211827/CallMeTeenTop.mp3",
            needDownload = true //表示等下载完之后再播放，便于后续复用，无延迟
         });
@@ -19,4 +28,34 @@ public class InnerAudio : Details
         });
     }
 
+
+    //暂停
+    public void pause()
+    {
+         Debug.Log("pause");
+         music.Pause();
+    }
+
+    //停止
+    public void stop()
+    {
+        Debug.Log("stop");
+        music.Stop();
+
+    }
+
+    //跳转
+    public void seek()
+    {
+
+        Debug.Log("seek");
+        music.Seek(3);
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("音频示例 OnDestroy");
+        music.Stop();
+        music.Destroy();
+    }
 }
